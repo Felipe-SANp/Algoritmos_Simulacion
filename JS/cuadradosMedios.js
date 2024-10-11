9332cfd6b29a34bb4b87c335313a073331ef80b5
Santiago <l22070087@valladolid.tecnm.mx>
2024-10-11 12:53:21 -0600

    codigo mas organizado..

--- JS/cuadradosMedios.js
+++ JS/cuadradosMedios.js
@@ -31,33 +31,18 @@ function cuadradosMedios(semilla, n) {
             alert('Algoritmo terminado.\n * Se detectó un bucle entre ' + XnMap[X_a] +
                 ' y X' + i +
                 '\n * se detiene la generación de números');
-
-            const fila = `
-            <tr id="result${i}" style="background-color:gray; color:white;">
-                <td>X<sub>${i}</sub></td> <!-- posicion de x -->
-                <td>${X_a}</td> <!-- valor de x -->
-                <td>${Y}</td> <!-- valor de x^2 -->
-                <td>${Xn}</td> <!-- valor de x extraido los 4 digitos-->
-                <td>${rn}</td> <!-- valor de r -->
-            </tr> `;
-            document.getElementById('t01').innerHTML += fila;
-            document.getElementById('result' + XnMap[X_a]).style.color = "white";
-            document.getElementById('result' + XnMap[X_a]).style.background = "rgb(137 137 137)";
+            
+            document.getElementById('t01').innerHTML += filaParametros(i, X_a, Y, Xn, rn);
+            styleID('result' + XnMap[X_a]);
+            styleID('result' + i);
             break;
         }
         if (n != null && i >= n) {
             break;
         }
 
-        const fila = `
-        <tr id="result${i}" style="background-color: white;">
-            <td>X<sub>${i}</sub></td> <!-- posicion de x -->
-            <td>${X_a}</td> <!-- valor de x -->
-            <td>${Y}</td> <!-- valor de x^2 -->
-            <td>${Xn}</td> <!-- valor de x extraido los 4 digitos-->
-            <td>${rn}</td> <!-- valor de r -->
-        </tr> `;
-        document.getElementById('t01').innerHTML += fila;
+        // agregar fila a la tabla de resultados
+        document.getElementById('t01').innerHTML += filaParametros(i, X_a, Y, Xn, rn);
 
         // Agregar Xn al mapa
         XnMap[X_a] = i;
@@ -77,17 +62,33 @@ document.getElementById('generarDatosBtn').addEventListener('click', function ()
 
     document.getElementById('t01').innerHTML =
         `<table id="t01" style="width: 860px; ">
-        <tr>
-            <th>X<sub>n</sub></th> <!-- posicion de x -->
-            <th>X<sub>i</sub></th> <!-- valor de x -->
-            <th>X<sub>i</sub><sup>2</sup></th> <!-- valor de x^2 -->
-            <th>X<sub>i</sub></th> <!-- valor de x extraido los 4 digitos-->
-            <th>r<sub>i</sub></th> <!-- valor de r -->
-        </tr>
-    </table>`;
+            <tr>
+                <th>X<sub>n</sub></th> <!-- posicion de x -->
+                <th>X<sub>i</sub></th> <!-- valor de x -->
+                <th>X<sub>i</sub><sup>2</sup></th> <!-- valor de x^2 -->
+                <th>X<sub>i</sub></th> <!-- valor de x extraido los 4 digitos-->
+                <th>r<sub>i</sub></th> <!-- valor de r -->
+            </tr>
+        </table>`;
 
     cuadradosMedios(semilla, n);
 });
+
+function styleID(id){
+    document.getElementById(id).style.color = "white";
+    document.getElementById(id).style.background = "gray";
+}
+
+function filaParametros(i, X_a, Y, Xn, rn){
+    return `
+    <tr id="result${i}" style="background-color: white;">
+        <td>X<sub>${i}</sub></td> <!-- posicion de x -->
+        <td>${X_a}</td> <!-- valor de x -->
+        <td>${Y}</td> <!-- valor de x^2 -->
+        <td>${Xn}</td> <!-- valor de x extraido los 4 digitos-->
+        <td>${rn}</td> <!-- valor de r -->
+    </tr> `;
+}
 
 function limpiarDatos() {
     document.getElementById('id-semilla').value = '';
