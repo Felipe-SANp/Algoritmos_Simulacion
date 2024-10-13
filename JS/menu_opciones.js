1681edddc449ed907f1e776d569d81dc003c48a0
Santiago <l22070087@valladolid.tecnm.mx>
2024-10-12 23:47:49 -0600

    Adiccion de boton para exportar en Excel

--- JS/menu_opciones.js
+++ JS/menu_opciones.js
@@ -25,17 +25,23 @@ function cambiarAlgoritmo() {
             break;
     }
 }
-// funcion para poder exportar la tabla en una imagen png 
-function exportarDatos() {
-    html2canvas(document.getElementById('t01'), {
-        onrendered: function (canvas) {
-            var link = document.createElement('a');
-            link.href = canvas.toDataURL();
-            link.download = 'semillas.png';
-            link.click();
-        }
-    });
-}
+
+// obtener el boton de exportar y la tabla
+const $btnExportar = document.querySelector("#btnExportar"),
+$tabla = document.querySelector("#t01");
+
+// evento click al boton exportar
+$btnExportar.addEventListener("click", function() {
+let tableExport = new TableExport($tabla, {
+    exportButtons: false, // No queremos botones
+    filename: "Numeros Pseudoaleatorios", //Nombre del archivo de Excel
+    sheetname: "Numeros Pseudoaletorios", //Título de la hoja
+});
+let datos = tableExport.getExportData();
+let preferenciasDocumento = datos.tabla.xlsx;
+tableExport.export2file(preferenciasDocumento.data, preferenciasDocumento.mimeType, preferenciasDocumento.filename, preferenciasDocumento.fileExtension, preferenciasDocumento.merges, preferenciasDocumento.RTL, preferenciasDocumento.sheetname);
+});
+
 // funcion dar otro estilo a una fila por id 
 function styleID(id) {
     document.getElementById(id).style.color = "white";
@@ -45,6 +51,3 @@ function styleID(id) {
 function alertBlucle(index, index_2){
     alert('Algoritmo terminado.\n * Se encontró un bucle entre X' + index +' y X' + index_2 +'\n * se detiene la generación de números');
 }
-
-document.getElementById("exportarBtn").addEventListener("click", exportarDatos);
-// document.getElementById("exportarBtn_").addEventListener("click", exportarDatos2);
